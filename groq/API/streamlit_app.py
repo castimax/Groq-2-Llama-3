3887d733a796ea3222799d0757d2ee7950c4c9f7
CASTIMAX <castimax@gmail.com>
2024-05-08 02:37:36 +0200

    Update streamlit_app.py
    Para preguntas y respuestas en español, con documentos de entrenamiento en inglés

--- groq/API/streamlit_app.py
+++ groq/API/streamlit_app.py
@@ -3,12 +3,13 @@ import requests
 
 st.title("Demostración de Chatgroq con Llama3")
 
-prompt1 = st.text_input("Ingrese su pregunta sobre los documentos")
+prompt1 = st.text_input("Ingrese su pregunta sobre los documentos (en español)")
 
 if st.button("Obtener respuesta"):
     response = requests.post("http://localhost:8000/ask", json={"question": prompt1})
     result = response.json()
 
+    st.write("Respuesta:")
     st.write(result['respuesta'])
 
     with st.expander("Búsqueda de documentos similares"):
