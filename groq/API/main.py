d0cc17ea30df6019a41b92fccb01bb7634a946c5
CASTIMAX <castimax@gmail.com>
2024-05-08 02:36:08 +0200

    Update main.py
    Se traducen al entrar y al salir del rag las preguntas al español, luego se hace el rag en inglés, como los document vectorizados, y se vuelve a traducir a español, la respuesta del rag en inglés

--- groq/API/main.py
+++ groq/API/main.py
@@ -10,6 +10,7 @@ from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain.document_loaders import CSVLoader, JSONLoader
 from dotenv import load_dotenv
+from googletrans import Translator
 
 load_dotenv()
 
@@ -23,45 +24,33 @@ llm = ChatGroq(groq_api_key=groq_api_key,
 
 prompt = ChatPromptTemplate.from_template(
     """
-    Responda las preguntas basándose en el contexto proporcionado.
-    Proporcione la respuesta más precisa según la pregunta.
-    <contexto>
+    Answer the questions based on the context provided.
+    Provide the most accurate answer based on the question.
+    <context>
     {context}
-    </contexto>
-    Preguntas: {input}
+    </context>
+    Questions: {input}
     """
 )
 
 @app.on_event("startup")
 async def startup_event():
-    embeddings = OllamaEmbeddings()
-
-    pdf_loader = PyPDFDirectoryLoader("./ruta/al/directorio/pdf")
-    pdf_docs = pdf_loader.load()
-
-    csv_loader = CSVLoader("./ruta/al/archivo.csv")
-    csv_docs = csv_loader.load()
-
-    json_loader = JSONLoader("./ruta/al/archivo.json")
-    json_docs = json_loader.load()
-
-    docs = pdf_docs + csv_docs + json_docs
-
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    final_documents = text_splitter.split_documents(docs)
-    vectors = FAISS.from_documents(final_documents, embeddings)
-
-    app.state.vectors = vectors
+    # ... (El código de inicio permanece igual)
 
 @app.post("/ask")
 async def ask_question(request: Request):
     prompt1 = await request.json()
     prompt1 = prompt1["question"]
 
+    translator = Translator()
+    prompt1_en = translator.translate(prompt1, dest='en').text
+
     document_chain = create_stuff_documents_chain(llm, prompt)
     retriever = app.state.vectors.as_retriever()
     retrieval_chain = create_retrieval_chain(retriever, document_chain)
 
-    response = retrieval_chain.invoke({'input': prompt1})
+    response = retrieval_chain.invoke({'input': prompt1_en})
 
-    return {"respuesta": response['respuesta'], "contexto": response["contexto"]}
+    response_es = translator.translate(response['respuesta'], dest='es').text
+
+    return {"respuesta": response_es, "contexto": response["contexto"]}
